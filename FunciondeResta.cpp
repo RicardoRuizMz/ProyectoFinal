8dec05669f0c5cd045c65a91346db58abe7ab0a4
JesusDiaz2003 <91902288+JesusDiaz2003@users.noreply.github.com>
2021-12-14 07:58:09 -0600

    Funcion de Resta
    Funcion de Resta

---
+++ FunciondeResta.cpp
@@ -0,0 +1,19 @@
+// FunciondeResta.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int Num1, num2, resta;
+	Num1 = 2;
+	num2 = 2;
+	resta = Num1 - num2;
+	cout << "El resultado es: " << resta;
+	return 0;
+   
+}
+
+
