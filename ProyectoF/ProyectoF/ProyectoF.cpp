fd532184c443576b59e57cfda72ce1971c3922c1
RicardoRuizMz <ricardo.ruiz00@comunidad.unam.mx>
2021-12-15 16:06:28 -0600

    Terminado

--- ProyectoF/ProyectoF/ProyectoF.cpp
+++ ProyectoF/ProyectoF/ProyectoF.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <iostream>
-
+#include "ProyectoF.h"
 int main(int argc,char*argv[])
 {
-    
+    //Mandar a llamar Funcion Resta
+    int Num1 = 2;
+    int Num2 = 8;
+    resta(Num1,Num2);
+    //Mandar a llamar Funcion Suma
+    int f = 5,
+        z = 6;
+    int resultado;
+    resultado=suma(f, z);
+    printf("\nEl resultado de la suma es: %i", resultado);
+    //Mandar a llamar Funcion Multiplica
+    int numero1 = 7;
+    int numero2 = 3;
+    resultado = func_multiplica(numero1, numero2);
+    printf("\nEl resultado de la multiplicacion es: %d", resultado);
 
 
+    return 0;
 }
---
+++ ProyectoF/ProyectoF/ProyectoF.h
@@ -0,0 +1,4 @@
+#pragma once
+int func_multiplica(int valor1, int valor2);
+int resta(int Num1, int Num2);
+int suma(int f, int z);
